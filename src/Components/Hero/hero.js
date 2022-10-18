bfd6406d18ce767d522ced6682d24671848234b4
Taha <tahasheikh682@gmail.com>
2022-10-18 19:15:53 +0500

    Initial commit

---
+++ src/Components/Hero/hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./hero.css"
+import four from "../../assets/four.jpg"
+import { Link } from 'react-router-dom'
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='mask'>
+<img className='into-img' src={four} />
+        </div>
+    <div className='content'>
+        <p>HI, I'M MUHAMMAD TAHA SHEIKH</p>
+   <h1>REACT DEVELOPER</h1>
+    
+    <Link to='/project' className='btn'>PROJECTS</Link>
+    <Link to='/contact' className='btn btn-light'>CONTACT</Link>
+        </div>
+        </div>
+  )
+}
+
+export default Hero
---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../Components/Hero/hero";
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero/>
+    </div>
+  );
+};
+
+export default Home;
